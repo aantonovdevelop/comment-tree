4c45cb1ec4db209cb8cd1186c9405a3683ae7308
Artem Antonov <aantonov.develop@gmail.com>
2016-05-15 00:18:01 +0500

    Remove unused function

--- schemes/comment.js
+++ schemes/comment.js
@@ -19,10 +19,6 @@ comment_schema.statics.get_parent = function (id) {
         .then((parent) => (parent) ? parent.parent : undefined);
 };
 
-comment_schema.statics.get_by_id = function (id) {
-    return this.model('Comment').findOne({_id: id});
-};
-
 comment_schema.statics.get_all = function () {
     return this.model('Comment').find();
 };
